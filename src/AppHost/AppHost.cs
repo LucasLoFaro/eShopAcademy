5e3c54486349f643e0a3a8e527ba7260ca4d2daa
Lucas Lo Faro <lucaslofaro@hotmail.com>
2025-09-01 19:49:22 -0300

    Adding aspire setup

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -1,7 +1,7 @@
 using Microsoft.Extensions.Hosting;
 using AppHost.Setup;
 using Aspire.Hosting.MongoDB;
-using Aspire.Hosting.PostgreSQL;
+using Aspire.Hosting.Postgres;
 using Aspire.Hosting.Redis;
 
 var builder = DistributedApplication.CreateBuilder(args);
@@ -10,78 +10,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 var api = builder.AddProject<Projects.API>("api");
 
 if(builder.Environment.IsDevelopment())
-    SetupDependencies.AddDevelopmentEnvironment(builder, api);
+    EnvironmentSetup.AddProjectDependencies(builder, api);
 
-// Backing services
-// Add Cassandra for the Product Catalog microservice
-var cassandra = builder.AddContainer("cassandra", "cassandra", "latest")
-    .WithEndpoint(port: 9042, targetPort: 9042, name: "cql")
-    .WithEnvironment("CASSANDRA_CLUSTER_NAME", "TestCluster")
-    .WithEnvironment("CASSANDRA_START_RPC", "true");
-
-// Add Redis for BasketService
-var redis = builder.AddRedis("redis");
-
-// Add MongoDB for StockService
-var mongo = builder.AddMongoDB("mongodb")
-    .AddDatabase("stock");
-
-// Add Service Bus emulator
-var serviceBus = builder.AddContainer("servicebus", "mcr.microsoft.com/azure-messaging/servicebus-emulator", "latest")
-    .WithEndpoint(port: 5672, targetPort: 5672, name: "amqp")  // AMQP
-    .WithEndpoint(port: 8081, targetPort: 8081, name: "http"); // Management
-
-// Add Postgres database for OrderService
-var postgres = builder.AddPostgres("eshopacademy-ordersdb")
-    .AddDatabase("orders");
-
-// Register microservice projects with their dependencies
-
-// BasketService API
-var basketApi = builder.AddProject("basket-api", "../BasketService/API/API.csproj")
-    .WithReference(redis)
-    .WithReference(serviceBus);
-
-// BasketService Events Processor
-var basketEvents = builder.AddProject("basket-events", "../BasketService/EventsProcessor/EventsProcessor/EventsProcessor.csproj")
-    .WithReference(redis)
-    .WithReference(serviceBus);
-
-// ProductCatalogService API
-var productApi = builder.AddProject("product-api", "../ProductCatalogService/Presentation/API/API.csproj")
-    .WithReference(cassandra)
-    .WithReference(serviceBus);
-
-// ProductCatalogService gRPC
-var productGrpc = builder.AddProject("product-grpc", "../ProductCatalogService/Presentation/gRPC/gRPC.csproj")
-    .WithReference(cassandra)
-    .WithReference(serviceBus);
-
-// OrderService API
-var orderApi = builder.AddProject("order-api", "../OrderService/Presentation/API/API.csproj")
-    .WithReference(postgres)
-    .WithReference(serviceBus);
-
-// OrderService Events Processor
-var orderEvents = builder.AddProject("order-events", "../OrderService/EventsProcessor/EventsProcessor/EventsProcessor.csproj")
-    .WithReference(postgres)
-    .WithReference(serviceBus);
-
-// StockService API
-var stockApi = builder.AddProject("stock-api", "../StockService/API/API.csproj")
-    .WithReference(mongo)
-    .WithReference(serviceBus);
-
-// StockService gRPC
-var stockGrpc = builder.AddProject("stock-grpc", "../StockService/gRPC/gRPC.csproj")
-    .WithReference(mongo)
-    .WithReference(serviceBus);
-
-// PaymentService gRPC
-var paymentGrpc = builder.AddProject("payment-grpc", "../PaymentService/gRPC/gRPC.csproj");
-
-// ShippingService gRPC
-var shippingGrpc = builder.AddProject("shipping-grpc", "../ShippingService/ShippingService/ShippingService.csproj");
 
 // Build and run the distributed application
 builder.Build().Run();
---
+++ src/AppHost/Setup/EnvironmentSetup.cs
@@ -0,0 +1,162 @@
+﻿using Aspire.Hosting;
+using Aspire.Hosting.Azure;
+using Microsoft.Extensions.Hosting;
+
+namespace AppHost.Setup;
+
+public static class EnvironmentSetup
+{
+    public static void AddProjectDependencies(
+        IDistributedApplicationBuilder builder,
+        IResourceBuilder<ProjectResource> api)
+    {
+        // Basket
+        var basketApi = builder.AddProject("eshopacademy-basket-api", "../BasketService/API/API.csproj");
+        var basketEvents = builder.AddProject("basket-events", "../BasketService/EventsProcessor/EventsProcessor/EventsProcessor.csproj");
+
+        // Product
+        var productApi = builder.AddProject("eshopacademy-product-api", "../ProductCatalogService/Presentation/API/API.csproj");
+        var productGrpc = builder.AddProject("product-grpc", "../ProductCatalogService/Presentation/gRPC/gRPC.csproj");
+
+        // Order
+        var orderApi = builder.AddProject("eshopacademy-order-api", "../OrderService/Presentation/API/API.csproj");
+        var orderEvents = builder.AddProject("order-events", "../OrderService/EventsProcessor/EventsProcessor/EventsProcessor.csproj");
+
+
+        // Stock
+        var stockApi = builder.AddProject("eshopacademy-stock-api", "../StockService/API/API.csproj");
+        var stockGrpc = builder.AddProject("stock-grpc", "../StockService/gRPC/gRPC.csproj");
+
+
+        // Payment
+        var paymentGrpc = builder.AddProject("eshopacademy-payment-grpc", "../PaymentService/gRPC/gRPC.csproj");
+
+        // Shipping
+        var shippingGrpc = builder.AddProject("eshopacademy-shipping-grpc", "../ShippingService/ShippingService/ShippingService.csproj");
+
+
+        if (builder.Environment.IsDevelopment())
+        {
+            // Backing services
+            IResourceBuilder<RedisResource> redis;
+            IResourceBuilder<MongoDBDatabaseResource> stockdb;
+            IResourceBuilder<PostgresDatabaseResource> ordersdb;
+            IResourceBuilder<ContainerResource> cassandra;
+            IResourceBuilder<ContainerResource> serviceBus;
+            IResourceBuilder<ContainerResource> wiremock;
+            SetupBackingServices(builder, out redis, out stockdb, out ordersdb, out cassandra, out serviceBus, out wiremock);
+
+            basketApi
+                .WithReference(redis)
+                .WaitFor(serviceBus)
+                .WithEnvironment("ConnectionStrings__ServiceBus",
+                                 "Endpoint=sb://eshopacademy-servicebus/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=dummyKey");
+
+            basketEvents
+                .WithReference(redis)
+                .WaitFor(serviceBus)
+                .WithEnvironment("ConnectionStrings__ServiceBus",
+                                 "Endpoint=sb://eshopacademy-servicebus/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=dummyKey");
+
+            productApi
+                .WaitFor(serviceBus)
+                .WaitFor(cassandra)
+                .WithEnvironment("ConnectionStrings__Cassandra", "Contact Points=eshopacademy-cassandra;Port=9042;")
+                .WithEnvironment("ConnectionStrings__ServiceBus",
+                                 "Endpoint=sb://eshopacademy-servicebus/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=dummyKey");
+
+            productGrpc
+                .WaitFor(serviceBus)
+                .WaitFor(cassandra)
+                .WithEnvironment("ConnectionStrings__Cassandra", "Contact Points=eshopacademy-cassandra;Port=9042;")
+                .WithEnvironment("ConnectionStrings__ServiceBus",
+                                 "Endpoint=sb://eshopacademy-servicebus/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=dummyKey");
+
+            orderApi
+                .WithReference(ordersdb)
+                .WaitFor(serviceBus)
+                .WithEnvironment("ConnectionStrings__ServiceBus",
+                                 "Endpoint=sb://eshopacademy-servicebus/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=dummyKey");
+
+            orderEvents
+                .WithReference(ordersdb)
+                .WaitFor(serviceBus)
+                .WithEnvironment("ConnectionStrings__ServiceBus",
+                                 "Endpoint=sb://eshopacademy-servicebus/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=dummyKey");
+
+            stockApi
+                .WithReference(stockdb)
+                .WaitFor(serviceBus)
+                .WithEnvironment("ConnectionStrings__ServiceBus",
+                                 "Endpoint=sb://eshopacademy-servicebus/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=dummyKey");
+
+            stockGrpc
+                .WithReference(stockdb)
+                .WaitFor(serviceBus)
+                .WithEnvironment("ConnectionStrings__ServiceBus",
+                                 "Endpoint=sb://eshopacademy-servicebus/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=dummyKey");
+
+            paymentGrpc
+                .WaitFor(wiremock)
+                .WithEnvironment("WireMock__BaseUrl", "http://wiremock:8080");
+
+            shippingGrpc
+                .WaitFor(wiremock)
+                .WithEnvironment("WireMock__BaseUrl", "http://wiremock:8080");
+        }
+
+        static void SetupBackingServices(
+            IDistributedApplicationBuilder builder, 
+            out IResourceBuilder<RedisResource> redis, 
+            out IResourceBuilder<MongoDBDatabaseResource> stockdb, 
+            out IResourceBuilder<PostgresDatabaseResource> ordersdb, 
+            out IResourceBuilder<ContainerResource> cassandra, 
+            out IResourceBuilder<ContainerResource> serviceBus, 
+            out IResourceBuilder<ContainerResource> wiremock)
+        {
+            redis = builder.AddRedis("eshopacademy-redis");
+            
+            stockdb = builder.AddMongoDB("eshopacademy-mongodb")
+                .AddDatabase("stock");
+            
+            ordersdb = builder.AddPostgres("eshopacademy-postgres")
+                .WithDataVolume()
+                .WithPgAdmin()
+                .AddDatabase("orders");
+
+            cassandra = builder.AddContainer("eshopacademy-cassandra", "cassandra", "latest")
+                .WithEndpoint(port: 9042, targetPort: 9042, name: "cql");
+
+            //serviceBus = builder.AddAzureServiceBus("eshopacademy-servicebus");
+
+            serviceBus = builder.AddContainer("eshopacademy-servicebus",
+                                  "mcr.microsoft.com/azure-messaging/servicebus-emulator",
+                                  "latest")
+                    .WithEndpoint(port: 5672, targetPort: 5672, name: "amqp")
+                    .WithEndpoint(port: 8081, targetPort: 8081, name: "http")
+                    .WithEnvironment("SERVICEBUS_CONNECTION",
+                        "Endpoint=sb://eshopacademy-servicebus/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=dummyKey")
+                    .WithEnvironment("ACCEPT_EULA","Y");
+
+            // External service mocks
+            var mappingsPath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "Mocks"));
+            wiremock = builder.AddContainer("eshopacademy-external-services-mock", "wiremock/wiremock:latest")
+                .WithEndpoint(8090, targetPort: 8080)
+                .WithBindMount(mappingsPath, "/home/wiremock/mappings");
+        }
+
+        //api.WithReference(wiremock);
+
+        // 🔹 RabbitMQ
+        //var rabbit = builder.AddRabbitMQ("rabbit")
+        //    .WithEndpoint(5672)
+        //    .WithManagementPlugin(); // acceso en http://localhost:15672
+
+        //api.WithReference(rabbit);
+
+        // Cassandra has been moved into the AppHost so that it can be
+        // referenced by multiple microservices alongside other backing
+        // resources (Redis, MongoDB and PostgreSQL). Only the WireMock
+        // external service mock is configured here for development.
+    }
+}
